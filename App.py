973a13bdc18a168259665d3017477efffdec6be9
carmelbe <carmelbe@post.bgu.ac.il>
2021-12-11 20:36:46 +0200

    7

---
+++ App.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/home")
+@app.route("/")
+def index_page():
+    return render_template('index.html')
+
+@app.route("/home_page")
+def home_page():
+    return redirect(url_for('index_page'))
+
+@app.route("/professional")
+def professional_page():
+    return render_template('cv.html')
+
+@app.route("/cv")
+def cv_page():
+    return redirect('/professional')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/CV.js
@@ -0,0 +1,10 @@
+function carmelFunction(){
+
+    var user={
+        firstName='Carmel',
+        lastName='Ben Shoshan',
+        email:'carmel.bens1@gmail.com',
+        age:25
+    }
+    console.log(user);
+}
