433c5a262c452ddae81a51ce48426a3d851749b3
carmelbe <carmelbe@post.bgu.ac.il>
2021-12-20 17:30:31 +0200

    8

--- App.py
+++ App.py
@@ -19,8 +19,17 @@ def professional_page():
 def cv_page():
     return redirect('/professional')
 
+@app.route("/Assignment8")
+def Assignment8_page():
+    name = 'Carmel'
+    last_name = 'Ben Shoshan'
+    return render_template('Assignment8.html',
+                           name=name,
+                           last_name=last_name,
+                           profile={'Favorite hobbie': 'to take pictures',
+                                    'Favorite music': 'Harry Styles',
+                                    'Favorite place': 'Home'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
