e19fea0316838d2fe7f3484152e7f101dfc6318e
carmelbe <carmelbe@post.bgu.ac.il>
2021-12-28 10:33:50 +0200

    9

--- App.py
+++ App.py
@@ -2,6 +2,15 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+users = {'user1': {'name': 'Yossi', 'email': 'yo@gmail.com'},
+         'user2': {'name': 'Carmel', 'email': 'carmel@gmail.com'},
+         'user3': {'name': 'Lior', 'email': 'lior@hotmail.com'},
+         'user4': {'name': 'Noam', 'email': 'noam@yahoo.com'},
+         'user5': {'name': 'ofir', 'email': 'ofir@mail.com'},
+         'user6': {'name': 'ofer', 'email': 'ofer@gmail.com'}
+         }
+
+
 @app.route("/home")
 @app.route("/")
 def index_page():
@@ -31,5 +40,29 @@ def Assignment8_page():
                                     'Favorite place': 'Home'})
 
 
+
+@app.route("/assignment9", methods=['GET', 'POST'])
+def assignment9_page():
+    # search form
+    if 'email' in request.args:
+        email = request.args['email']
+        if email == '':
+            return render_template('assignment9.html', user_list=users)
+        # search it in users dict
+        for key, value in users.items():
+            if value.get('email') == email:
+                return render_template('assignment9.html', u_name=value.get('name'), u_email=value.get('email'))
+    # registration form
+    if request.method == "POST":
+        session['username'] = request.form['username']
+    return render_template('assignment9.html')
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout_func():
+    session['username'] = ''
+    return render_template('assignment9.html')
+
+
 if __name__ == '__main__':
+    app.secret_key = '123'
     app.run(debug=True)
